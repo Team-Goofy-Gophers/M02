f1efc984c4556cb645371bb96c1e542863d979cd
prabhuomkar9 <prabhuomkar9@gmail.com>
2025-04-08 02:17:36 +0530

    init: create-t3-app + basic schema

---
+++ ui/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import { auth } from "~/server/auth";
+import { HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <HydrateClient>
+      <main></main>
+    </HydrateClient>
+  );
+}
---
+++ ui/src/server/api/routers/user.ts
@@ -0,0 +1,15 @@
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  me: protectedProcedure
+    .query(({ ctx }) => {
+      return ctx.db.user.findFirst({
+        where: {
+          id: ctx.session.user.id,
+        },
+      })
+    }),
+});
